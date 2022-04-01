0f5fc179375ea1fd19ab90562a27091532b3e052
SONGBRO <songbro@songbro.com>
2022-04-01 14:51:18 +0900

    C 문법 026

---
+++ CodeUp_C 100/CodeUp-026.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+int main() {
+
+    unsigned int n;
+    scanf("%u", &n);
+
+    printf("%u", n);
+
+    return 0;
+}
+
+// 아래도 가능
+#include <stdio.h>
+
+int main() {
+
+    int n;
+    scanf("%u", &n);
+
+    printf("%u", n);
+
+    return 0;
+}
