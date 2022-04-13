c3b7104f0051c0c1c07d2b6c493899b5b5618cf9
SONGBRO <songbro@songbro.com>
2022-04-13 16:41:49 +0900

    C 문법 065

---
+++ CodeUp_C 100/CodeUp-065.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+
+int main() {
+
+    int a;
+    scanf("%d", &a);
+    if(a<0) {
+
+        printf("minus\n");
+        if(a%2==0) {
+
+            printf("even\n");
+        } else {
+
+            printf("odd\n");
+        }
+    } else {
+
+        printf("plus\n");
+                if(a%2==0) {
+
+            printf("even\n");
+        } else {
+
+            printf("odd\n");
+        }
+    }
+
+    return 0;
+}
