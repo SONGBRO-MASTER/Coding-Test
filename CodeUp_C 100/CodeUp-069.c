9c815f87a13be8986a65e1c8170ed91c4ececc1c
SONGBRO <songbro@songbro.com>
2022-04-14 10:41:39 +0900

    C 문법 069

---
+++ CodeUp_C 100/CodeUp-069.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main() {
+
+    int n;
+    reload:
+        scanf("%d", &n);
+        if(n!=0) {
+
+            printf("%d\n", n);
+            goto reload;
+        } 
+        
+    return 0;
+}
