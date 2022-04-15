b0f6b69c75b1806e719a4d929d90e5c6f3b6d640
SONGBRO <songbro@songbro.com>
2022-04-15 09:34:09 +0900

    C 문법 081

---
+++ CodeUp_C 100/CodeUp-081.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+int main() {
+
+    int r, g, b;
+    int i, j, k;
+    int sum = 0;
+
+    scanf("%d %d %d", &r, &g, &b);
+
+    for(i=0; i<r; i++) {
+        for(j=0; j<g; j++) {
+            for(k=0; k<b; k++) {
+
+                printf("%d %d %d\n", i, j, k);
+                sum++;
+            }
+        }
+    }
+    printf("%d", sum);
+
+    return 0;
+}
